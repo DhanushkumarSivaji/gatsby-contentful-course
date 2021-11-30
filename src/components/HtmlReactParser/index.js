4c91268a5800113f48958d07165753a44df118d6
Dhanush kumar Sivaji <dhanushkumarsivaji@gmail.com>
2021-11-30 21:18:38 +0530

    removed cleanHtml func

--- src/components/HtmlReactParser/index.js
+++ src/components/HtmlReactParser/index.js
@@ -1,19 +1,10 @@
 import React from "react";
-import DOMPurify from "dompurify";
 import parse from "html-react-parser";
 
-const HtmlReactParser = ({htmlContent=""}) => {
-
-const dirtyHTML = htmlContent;
-  // The library allows HTML, SVG and MathML
-// We only need HTML
-const cleanHTML = DOMPurify.sanitize(dirtyHTML, {
-    USE_PROFILES: { html: true },
-  });
-  
+const HtmlReactParser = ({htmlContent=""}) => {  
     return (
     <>
-    {parse(cleanHTML)}
+    {parse(htmlContent)}
     </>
     )
 };
